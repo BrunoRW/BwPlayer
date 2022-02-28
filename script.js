4126816414e60ec28c2e32172be69b33a88e0ec9
Bruno Ricardo Wotzke <49734747+BrunoRW@users.noreply.github.com>
2022-02-28 13:37:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ const head = document.querySelectorAll("head")[0];
 // local create bplayer 
 const local_create_bplayer = document.querySelectorAll(`${local_bplayer}`)[0];
 local_create_bplayer.style.position = "relative";
+local_create_bplayer.style.overflow = "hidden";
+local_create_bplayer.style.background = "black";
 
 // link style css
 const link_style = document.createElement("link");
 link_style.rel = "stylesheet";
-link_style.href = "style.css";
+link_style.href = "//bwplayer.ga/api/style.css";
 head.append(link_style);
 
 
@@ -50,8 +52,8 @@ tempo_span.innerText = "0:00 / 0:00";
 out_sets.append(tempo_span);
 
 // play icon
-const play_icon = '<svg class="icon" xmlns="http://www.w3.org/2000/svg" class="jw-svg-icon jw-svg-icon-play" viewBox="0 0 240 240" focusable="false"><path d="M62.8,199.5c-1,0.8-2.4,0.6-3.3-0.4c-0.4-0.5-0.6-1.1-0.5-1.8V42.6c-0.2-1.3,0.7-2.4,1.9-2.6c0.7-0.1,1.3,0.1,1.9,0.4l154.7,77.7c2.1,1.1,2.1,2.8,0,3.8L62.8,199.5z"></path></svg>';
-const pause_icon = '<svg class="icon" xmlns="http://www.w3.org/2000/svg" class="jw-svg-icon jw-svg-icon-pause" viewBox="0 0 240 240" focusable="false"><path d="M100,194.9c0.2,2.6-1.8,4.8-4.4,5c-0.2,0-0.4,0-0.6,0H65c-2.6,0.2-4.8-1.8-5-4.4c0-0.2,0-0.4,0-0.6V45c-0.2-2.6,1.8-4.8,4.4-5c0.2,0,0.4,0,0.6,0h30c2.6-0.2,4.8,1.8,5,4.4c0,0.2,0,0.4,0,0.6V194.9z M180,45.1c0.2-2.6-1.8-4.8-4.4-5c-0.2,0-0.4,0-0.6,0h-30c-2.6-0.2-4.8,1.8-5,4.4c0,0.2,0,0.4,0,0.6V195c-0.2,2.6,1.8,4.8,4.4,5c0.2,0,0.4,0,0.6,0h30c2.6,0.2,4.8-1.8,5-4.4c0-0.2,0-0.4,0-0.6V45.1z"></path></svg>';
+const play_icon = '<svg class="icon-player" xmlns="http://www.w3.org/2000/svg" class="jw-svg-icon jw-svg-icon-play" viewBox="0 0 240 240" focusable="false"><path d="M62.8,199.5c-1,0.8-2.4,0.6-3.3-0.4c-0.4-0.5-0.6-1.1-0.5-1.8V42.6c-0.2-1.3,0.7-2.4,1.9-2.6c0.7-0.1,1.3,0.1,1.9,0.4l154.7,77.7c2.1,1.1,2.1,2.8,0,3.8L62.8,199.5z"></path></svg>';
+const pause_icon = '<svg class="icon-player" xmlns="http://www.w3.org/2000/svg" class="jw-svg-icon jw-svg-icon-pause" viewBox="0 0 240 240" focusable="false"><path d="M100,194.9c0.2,2.6-1.8,4.8-4.4,5c-0.2,0-0.4,0-0.6,0H65c-2.6,0.2-4.8-1.8-5-4.4c0-0.2,0-0.4,0-0.6V45c-0.2-2.6,1.8-4.8,4.4-5c0.2,0,0.4,0,0.6,0h30c2.6-0.2,4.8,1.8,5,4.4c0,0.2,0,0.4,0,0.6V194.9z M180,45.1c0.2-2.6-1.8-4.8-4.4-5c-0.2,0-0.4,0-0.6,0h-30c-2.6-0.2-4.8,1.8-5,4.4c0,0.2,0,0.4,0,0.6V195c-0.2,2.6,1.8,4.8,4.4,5c0.2,0,0.4,0,0.6,0h30c2.6,0.2,4.8-1.8,5-4.4c0-0.2,0-0.4,0-0.6V45.1z"></path></svg>';
 bts_pp.innerHTML = play_icon;
 
 // slider
@@ -115,11 +117,13 @@ local_create_bplayer.onmousemove = ()=> {
 }
 
 const defineEvt = setInterval(()=> {
-    if(checkEvt(3000) && video.currentTime != 0){
+    if(checkEvt(2000) && video.currentTime != 0){
         out_sets.style.opacity = 0;
+        out_sets.style.transform = "translateY(100%)";
         local_create_bplayer.style.cursor = "none";
     } else {
         out_sets.style.opacity = 1;
+        out_sets.style.transform = "translateY(0)";
         local_create_bplayer.style.cursor = "default";
     }
 },100)
